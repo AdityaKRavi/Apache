a729d27c7dad8f58a1291c6a102ae1ebb862cd65
AdityaKRavi <ravi981957632@gmail.com>
2018-12-21 07:19:59 +0000

    Added node info

---
+++ recipes/server.rb
@@ -0,0 +1,16 @@
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content "<h1>Hello World</h1>
+    HOSTNAME : #{node['hostname']}
+    IPADDRESS : #{node['ipaddress']}
+    CPU : #{node['cpu']['0']['mhz']}
+    MEMORY : #{node['memory']['total']}
+  "
+end
+
+service 'httpd' do 
+  action [:enable, :start]
+end
