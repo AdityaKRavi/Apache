7e84e13887bcbc3d28703cbd79698215601ce769
AdityaKRavi <ravi981957632@gmail.com>
2018-12-24 07:15:52 +0000

    Added notification and template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,15 +2,23 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello World</h1>
-    HOSTNAME : #{node['hostname']}
-    IPADDRESS : #{node['ipaddress']}
-    CPU : #{node['cpu']['0']['mhz']}
-    MEMORY : #{node['memory']['total']}
-  "
+template '/etc/motd' do
+  source 'motd.erb'
+  variables(
+    :name => 'adityakravi'
+  )
+end
+
+#remote_file 'var/www/html/adi.jpg' do
+#  source 'https://www.shutterstock.com/image-vector/cute-cartoon-kids-writing-test-classroom-364684505.jpg'
+#end
+
+template 'var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+  notifies :restart ,'service[httpd]', :immediately
 end
 
 service 'httpd' do 
-  action [:enable, :start]
+   action [:enable, :start]
 end
